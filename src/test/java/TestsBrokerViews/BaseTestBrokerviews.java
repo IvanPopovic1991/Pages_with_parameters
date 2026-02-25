b761baf1ad2b222db125c1ac18edd10e1664ffb9
Vlajs <vladimirspasic1996@gmail.com>
2026-02-25 09:23:29 +0100

    added new Broker Views page, class, testNG (added checkBox method)

---
+++ src/test/java/TestsBrokerViews/BaseTestBrokerviews.java
@@ -0,0 +1,22 @@
+package TestsBrokerViews;
+
+import Selenium_Core.DriverManager;
+import Selenium_Core.DriverManagerFactory;
+import org.openqa.selenium.WebDriver;
+
+public class BaseTestBrokerviews {
+
+
+    WebDriver driver;
+    DriverManager driverManager;
+
+    public void baseSetUp(String browser, String version){
+        driverManager = DriverManagerFactory.getDriverManager(browser);
+        driver =driverManager.getWebDriver(version);
+        driver.get("https://dlp.brokereviews.com/lps/pro-dark/en?fts=age-annual-saving-knowledge-plang:all");
+    }
+
+    public void baseTearDown(){
+        driverManager.quitDriver();
+    }
+}
